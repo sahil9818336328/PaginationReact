5fb5adc5acbd6b6fd72f46279e09804f891544c6
sahil <sahilkeshav1997@gmail.com>
2021-09-13 17:48:00 +0530

    feat: added preloader

--- src/App.js
+++ src/App.js
@@ -1,78 +1,98 @@
-import React, { useState, useEffect } from "react";
-import { useFetch } from "./useFetch";
-import User from "./User";
+import React, { useState, useEffect } from 'react'
+import { useFetch } from './useFetch'
+import User from './User'
+import $ from 'jquery'
 
 const App = () => {
-  const { loading, data } = useFetch();
+  const [isloading, setIsLoading] = useState(true)
+  useEffect(() => {
+    $(function () {
+      $('.loader').delay(2000).fadeOut('slow')
+      $('#overlayer').delay(2000).fadeOut('slow')
+      setIsLoading(false)
+    })
+  }, [])
+  const { loading, data } = useFetch()
   // console.log(data);
-  const [page, setPage] = useState(0);
-  const [followers, setFollowers] = useState([]);
+  const [page, setPage] = useState(0)
+  const [followers, setFollowers] = useState([])
 
   useEffect(() => {
-    if (loading) return;
-    setFollowers(data[page]);
-  }, [loading, data, page]);
+    if (loading) return
+    setFollowers(data[page])
+  }, [loading, data, page])
 
   const prevBtn = () => {
     setPage((prevValue) => {
-      let newPage = prevValue - 1;
+      let newPage = prevValue - 1
       if (newPage < 0) {
-        newPage = data.length - 1;
+        newPage = data.length - 1
       }
-      return newPage;
-    });
-  };
+      return newPage
+    })
+  }
 
   const handleClick = (index) => {
-    setPage(index);
-  };
+    setPage(index)
+  }
 
   const nextBtn = () => {
     setPage((prevValue) => {
-      let newPage = prevValue + 1;
+      let newPage = prevValue + 1
       if (newPage > data.length - 1) {
-        newPage = 0;
+        newPage = 0
       }
-      return newPage;
-    });
-  };
+      return newPage
+    })
+  }
   return (
-    <main>
-      <div className="section-title">
-        <h1>{loading ? "loading..." : "pagination"}</h1>
+    <>
+      <div id='overlayer'>
+        <span className='loader'>
+          <span className='loader-inner'></span>
+        </span>
       </div>
+      {!isloading && (
+        <main>
+          <div className='section-title'>
+            <h1>{loading ? 'loading...' : 'pagination'}</h1>
+          </div>
 
-      <section className="followers">
-        <div className="container">
-          {followers.map((follower) => {
-            return <User key={follower.id} {...follower} />;
-          })}
-        </div>
-      </section>
+          <section className='followers'>
+            <div className='container'>
+              {followers.map((follower) => {
+                return <User key={follower.id} {...follower} />
+              })}
+            </div>
+          </section>
 
-      {!loading && (
-        <div className="btn-container">
-          <button className="prev-btn" onClick={prevBtn}>
-            prev
-          </button>
-          {data.map((btn, index) => {
-            return (
-              <button
-                key={index}
-                className={`page-btn ${index === page ? "active-btn" : null}`}
-                onClick={() => handleClick(index)}
-              >
-                {index + 1}
+          {!loading && (
+            <div className='btn-container'>
+              <button className='prev-btn' onClick={prevBtn}>
+                prev
               </button>
-            );
-          })}
-          <button className="next-btn" onClick={nextBtn}>
-            next
-          </button>
-        </div>
+              {data.map((btn, index) => {
+                return (
+                  <button
+                    key={index}
+                    className={`page-btn ${
+                      index === page ? 'active-btn' : null
+                    }`}
+                    onClick={() => handleClick(index)}
+                  >
+                    {index + 1}
+                  </button>
+                )
+              })}
+              <button className='next-btn' onClick={nextBtn}>
+                next
+              </button>
+            </div>
+          )}
+        </main>
       )}
-    </main>
-  );
-};
+    </>
+  )
+}
 
-export default App;
+export default App
